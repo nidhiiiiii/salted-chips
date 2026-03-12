9bb33822f6718730172c63111fccab248ed4c5f5
nidhiiiiii <knnidhi81@gmail.com>
2026-03-12 23:52:21 +0530

    changed the workers and migration files

--- conftest.py
+++ conftest.py
@@ -6,9 +6,13 @@ import asyncio
 import pytest
 
 
+
+
 @pytest.fixture(scope="session")
 def event_loop():
     """Use a single event loop for all async tests."""
     loop = asyncio.new_event_loop()
     yield loop
     loop.close()
+
+
